aa72645564db0e692679d70aaa929aa98b5c45f3
d460 <dagovf@comunidad.unam.mx>
2017-04-03 00:07:05 -0500

    Listo python

--- primer/Lecture.py
+++ primer/Lecture.py
@@ -1,8 +1,27 @@
 import simplekml
 import pandas
+import tkinter
+from tkinter.filedialog import askopenfilename
 
-df = pandas.read_csv("Coordenadas.csv")
-kml = simplekml.Kml()
-for lon, lat in zip(df["Longitud"], ["Latitud"]):
-    kml.newpoint(coords=[(lon, lat)])
-kml.save("Puntos.kml")
+def browse():
+    global infile
+    infile=askopenfilename()
+
+
+def kmlfuntion(outfile="Puntos.kml"):
+    df = pandas.read_csv(infile)
+    kml = simplekml.Kml()
+    for lon, lat in zip(df["Longitud"], ["Latitud"]):
+        kml.newpoint(coords=[(lon, lat)])
+    kml.save(outfile)
+
+root = tkinter.Tk()
+root.title("KML Generator")
+label = tkinter.Label(root, text="Este programa genera un archivo KML")
+label.pack()
+button1 = tkinter.Button(root, text="Browse", command=browse)
+button1.pack()
+kmlbutton = tkinter.Button(root, text="Generate KML", command=kmlfuntion)
+kmlbutton.pack()
+root.mainloop()
+
